6f882dc0bcd29a907fa79a514810ff95abf90cba
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-30 09:34:32 +0530

    feat: add cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
-const addItemToCartHandler = (item) => {};
-const removeItemFromCartHandler = (id) => {};
 
-const cartContext = {
+const defaultCartState = {
   items: [],
-  total: 0,
-  addItem: addItemToCartHandler,
-  removeItem: removeItemFromCartHandler,
+  totalAmount: 0,
 };
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItem = state.items.concat(action.item);
+     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+     return {
+      items: updatedItem,
+      totalAmount: updatedTotalAmount
+     }
+  } else if (action.type === "REMOVE") {
+  }
+  return defaultCartState;
+};
+
 const CartProvider = ({ children }) => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+  const addItemToCartHandler = (item) => {
+    dispatchCart({ type: "ADD", item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCart({ type: "REMOVE", id });
+  };
+  const cartContext = {
+    items: [],
+    total: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
