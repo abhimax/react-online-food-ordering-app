42e86c69c76d2a515573e27a5388845fbedff7cb
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-04 20:24:04 +0530

    feat: add functionality to update cart batch count

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,16 +1,29 @@
+import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
-const MealItem = ({name, description, price}) => {
-    const mealPrice = `$${price.toFixed(2)}`;
-    return <li className={classes.meal}>
-        <div>
-            <h3>{name}</h3>
-            <div className={classes.description}>{description}</div>
-            <div className={classes.price}>{mealPrice}</div>
-        </div>
-        <div>
-            <MealItemForm/>
-        </div>
+import CartContext from "../../store/cart-context";
+const MealItem = ({ id, name, description, price }) => {
+  const cartContext = useContext(CartContext);
+  const mealPrice = `$${price.toFixed(2)}`;
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id: id,
+      name: name,
+      price: price,
+      amount: amount,
+    });
+  };
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{mealPrice}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
     </li>
+  );
 };
 export default MealItem;
