026f67cccadb23fd6d16062774652069f8450d8b
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-05 19:18:31 +0530

    fix: fix total amount issue in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,19 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 const Cart = ({onCartClose}) => {
+
+
+  const cartCtx = useContext(CartContext);
+  //const totalAmount = cartCtx.totalAmount;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  console.log('cartContext.totalAmount >>',cartCtx.totalAmount)
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 13.25 }].map((item) => {
-        return <li>{item.name}</li>;
+      {cartCtx.items.map((item) => {
+        console.log('item >>',item)
+        return <li key={item.id}>{item.name}</li>;
       })}
     </ul>
   );
@@ -13,6 +22,7 @@ const Cart = ({onCartClose}) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={onCartClose}>Close</button>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,49 +1,47 @@
-import { useReducer } from "react";
-import CartContext from "./cart-context";
+import { useReducer } from 'react';
+
+import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0,
+  totalAmount: 0
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
+  if (action.type === 'ADD') {
     const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount,
+      totalAmount: updatedTotalAmount
     };
-  } else if (action.type === "REMOVE") {
-    const existingCartItemIndex = state.item.findIndex(
-      (item) => item.id === action.item.id
-    );
   }
   return defaultCartState;
 };
 
-const CartProvider = ({ children }) => {
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    defaultCartState
-  );
+const CartProvider = ({children}) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: "ADD", item: item });
+    dispatchCartAction({type: 'ADD', item: item});
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", id: id });
+    dispatchCartAction({type: 'REMOVE', id: id});
   };
 
   const cartContext = {
     items: cartState.items,
-    total: cartState.totalAmount,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
-    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
+      {children}
+    </CartContext.Provider>
   );
 };
+
 export default CartProvider;
