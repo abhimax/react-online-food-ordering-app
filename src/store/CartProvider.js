ae55b540f7e97418fba63e9116211f0b989a13bb
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-07 08:45:59 +0530

    feat: add item qty update method to avoid dup

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,8 +9,23 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex( item => item.id === action.item.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
+    let updatedItems;
+    if(existingCartItem){
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }else{
+      updatedItem = { ...action.item};
+      updatedItems = state.items.concat(updatedItem);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
