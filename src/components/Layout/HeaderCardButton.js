8a5d8a5acf4ef2a823b97d1b3d42e0cddd48fa78
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-10-08 09:02:04 +0530

    feat: add cart btn animation with actions

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIco";
 import classes from "./HeaderCartButton.module.css";
-const HeaderCartButton = ({onClick}) => {
-    const CartCtx = useContext(CartContext);
-    const numberOfCartItems = CartCtx.items.reduce((curNumber, item) => { return curNumber + item.amount},0);
-    return(
-        <button className={classes.button} onClick={onClick}>
-            <span className={classes.icon}>
-                <CartIcon/>
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    );
-}
+const HeaderCartButton = ({ onClick }) => {
+  const CartCtx = useContext(CartContext);
+  let { items } = CartCtx;
+  const numberOfCartItems = CartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  const [isBtnBump, setBtnBump] = useState(false);
+  const btnClass = `${classes.button} ${isBtnBump && classes.bump}`;
+  useEffect(() => {
+    setBtnBump(true);
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClass} onClick={onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 export default HeaderCartButton;
